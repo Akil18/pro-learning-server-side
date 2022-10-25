7d9f8012b039c56aa2a509527ab118831c3de795
Akil Chowdhury <akiluddinchy@gmail.com>
2022-10-26 03:33:35 +0600

    added data and route courses

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const courses = require('./data/courses.json');
+
 app.get('/', (req, res) => {
     res.send('server is up');
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
